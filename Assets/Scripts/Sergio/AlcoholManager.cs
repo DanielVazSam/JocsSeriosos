23ba6e6788c6be9e10d9586593bd9dc4906eff2a
Sergio Chinchilla Córdoba <60751524+Sergio-Chinchilla@users.noreply.github.com>
2022-11-28 15:30:16 +0100

    "Animacion"

--- Assets/Scripts/Sergio/AlcoholManager.cs
+++ Assets/Scripts/Sergio/AlcoholManager.cs
@@ -13,6 +13,8 @@ public class AlcoholManager : MonoBehaviour, IMinigameFunctionsInterface
 
     public Text text;
     public GameObject multa;
+    public GameObject enemic;
+    public Sprite[] enemicSprites;
     public bool mentir = false;
     public static float fiabilitatAlcoholimetre = 100;
     public float decrementAlcoholimetre = 5;
@@ -26,7 +28,9 @@ public class AlcoholManager : MonoBehaviour, IMinigameFunctionsInterface
     private float[] minMaxError = { 0.5f, 5f };
     private int numCiutada = 0;
     private int nRespostes;
+    private float[] positions = { -11.43f, -3.12f };
     private int nEncerts = 0;
+    private bool isDoingAnimation = false;
 
     void Start()
     {
@@ -43,43 +47,50 @@ public class AlcoholManager : MonoBehaviour, IMinigameFunctionsInterface
     //FER CONTROL
     public void FunctionAction1()
     {
-                      // consumicions * quantitat d'alcohol per consumició * graus d'alcohol 
-        float result = newPerson.quantity * newPersonAlcohol.quantity * newPersonAlcohol.degrees;
-        float encert = Random.Range(0f, 100f);
-        if (encert > fiabilitatAlcoholimetre)
+        if(!isDoingAnimation)
         {
-            Debug.Log("Error!");
-            result += Random.Range(minMaxError[0], minMaxError[1]);
-        }
-        result = Mathf.Round(result * 100f) / 100f;
-        text.text = "L'acoholímetre ha donat: " + result + "%";
+            // consumicions * quantitat d'alcohol per consumició * graus d'alcohol 
+            float result = newPerson.quantity * newPersonAlcohol.quantity * newPersonAlcohol.degrees;
+            float encert = Random.Range(0f, 100f);
+            if (encert > fiabilitatAlcoholimetre)
+            {
+                Debug.Log("Error!");
+                result += Random.Range(minMaxError[0], minMaxError[1]);
+            }
+            result = Mathf.Round(result * 100f) / 100f;
+            text.text = "L'acoholímetre ha donat: " + result + "%";
 
-        fiabilitatAlcoholimetre -= decrementAlcoholimetre;
+            fiabilitatAlcoholimetre -= decrementAlcoholimetre;
+        } 
     }
 
     //TORNAR A DEMANAR
     public void FunctionAction2()
     {
-        int quantity = (int)newPerson.quantity - newPerson.fakeValue;
-        text.text = quantity == 0 ? "Bones! Jo no he begut" : "Bones! Jo he begut " + quantity + " de " + newPersonAlcohol.name;
+        if (!isDoingAnimation)
+        {
+            int quantity = (int)newPerson.quantity - newPerson.fakeValue;
+            text.text = quantity == 0 ? "Bones! Jo no he begut" : "Bones! Jo he begut " + quantity + " de " + newPersonAlcohol.name;
+        }
     }
 
     //DEIXAR PASSAR
     public void FunctionAction3()
     {
-        multar(3);
+        if(!isDoingAnimation) multar(3);
     }
 
     //MULTAR
     public void FunctionAction4()
     {
-        multa.SetActive(true);
+        if(!isDoingAnimation) multa.SetActive(true);
     }
 
     //Funció que evalua la correctesa de la decisió del jugador.
     //Es valora que multi a la persona per les coses que ha fet malament i que no multi per una cosa que no ha fet
     public void multar(int cas)
     {
+        isDoingAnimation = true;
         multa.SetActive(false);
         float alcoholValue = newPerson.quantity * newPersonAlcohol.quantity * newPersonAlcohol.degrees;
         switch (cas)
@@ -101,15 +112,17 @@ public class AlcoholManager : MonoBehaviour, IMinigameFunctionsInterface
                 if (alcoholValue <= MAX_ALCOHOL) nEncerts++;
                 break;
         }
-        GenerateNewPerson();
+        StartCoroutine(Move(0));
     }
 
     //Funció que genera nou perfil de persona segons si ha begut o no
     private void GenerateNewPerson()
     {
-        
+        isDoingAnimation = true;
         if (numCiutada < maxCiutadans)
         {
+            enemic.GetComponent<SpriteRenderer>().sprite = enemicSprites[Random.Range(0, 3)];
+
             int i = Random.Range(0, alcohols.Count);
             newPersonAlcohol = alcohols[i];
 
@@ -124,7 +137,22 @@ public class AlcoholManager : MonoBehaviour, IMinigameFunctionsInterface
         else
             Debug.Log($"Acabat! Puntuació: {nEncerts}/{nRespostes}");
         numCiutada++;
+        StartCoroutine(Move(1));
+    }
 
+    //Esquerra: 0, Dreta: 1
+    private IEnumerator Move(int axis)
+    {
+        if(axis == 0) enemic.GetComponent<SpriteRenderer>().flipX = true;
+        else enemic.GetComponent<SpriteRenderer>().flipX = false;
+        while((axis == 0 && enemic.transform.position.x > positions[axis]) || (axis == 1 && enemic.transform.position.x < positions[axis]))
+        {
+            float val = axis == 0 ? -0.1f : 0.1f;
+            enemic.transform.position += new Vector3(val, 0, 0);
+            yield return new WaitForSeconds(0.01f);
+        }
+        if(axis == 0) GenerateNewPerson();
+        else isDoingAnimation = false;
     }
 
     private void mostrarLlista()
